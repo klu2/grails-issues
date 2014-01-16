ad66a27bc5f02a6ff7da5b29be86557350bd05a0
klehner <klaus.lehner@catalysts.cc>
2014-01-16 21:23:57 +0100

    upgrade to grails 2.3.5

--- grails-issues/grails-app/conf/BuildConfig.groovy
+++ grails-issues/grails-app/conf/BuildConfig.groovy
@@ -7,6 +7,8 @@ grails.project.target.level = 1.7
 grails.project.source.level = 1.7
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+
+
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -34,14 +36,14 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.47"
+        build ":tomcat:7.0.50"
 
         // plugins for the compile step
         compile ":scaffolding:2.0.1"
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.6" // or ":hibernate4:4.1.11.2"
+        runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.2"
         runtime ":database-migration:1.3.8"
         runtime ":jquery:1.10.2"
         runtime ":resources:1.2.1"
